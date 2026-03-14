5c8ffe152dc2ac27ed2ca474a1a890e51ee90f8d
bunkheangheng <bunkheangheng99@gmail.com>
2026-03-14 12:40:24 +0700

    change to normal way

--- src/server.ts
+++ src/server.ts
@@ -19,26 +19,9 @@ const PORT = process.env.PORT ? parseInt(process.env.PORT) : 3333;
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.static(join(ROOT, "public")));
 
 let scraping = false;
-
-app.get("/", (_req, res) => {
-  res.sendFile(join(ROOT, "index.html"));
-});
-
-// Firebase config for local dev (so UI works with Express or static + Firebase)
-app.get("/config.js", (_req, res) => {
-  const config = {
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.FIREBASE_APP_ID,
-  };
-  res.setHeader("Content-Type", "application/javascript");
-  res.send(`window.FIREBASE_CONFIG = ${JSON.stringify(config)};`);
-});
 
 // --- API Routes ---
 
